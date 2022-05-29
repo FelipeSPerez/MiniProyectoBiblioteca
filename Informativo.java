7c3a358574c572f823c1af8963a190221e391d1b
Javier Jara <106366144+JavierJARA14@users.noreply.github.com>
2022-05-29 17:44:50 -0600

    Update Informativo.java

--- Informativo.java
+++ Informativo.java
@@ -4,9 +4,32 @@ public class Informativo extends Libro {
     
     private String fechaPublicacion;
 
+    //CONSTRUCTORES
     public Informativo(String titulo, String nombreAutor, String isbn, String editorial, String condicion, String fechaPublicacion) {
         super(titulo, nombreAutor, isbn, editorial, condicion);
         this.fechaPublicacion = fechaPublicacion;
+    }
+    
+    //GETTERS
+
+    public String getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+    
+   
+    
+ //TOSTRING
+    public String displayLibros() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.displayLibros());
+        sb.append("Informativo{");
+        sb.append("fechaPublicacion=").append(fechaPublicacion);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    public String EntradaLibros() {
+        return super.EntradaLibros() + "Informativo{" + "fechaPublicacion=" + fechaPublicacion + '}';
     }
     
 }
